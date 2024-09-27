e7173c0db32148ba5754d83fb69c4e0b07aa9c85
ale94 <alejandrorua111@gmail.com>
2024-09-27 19:15:58 -0300

    feat: entity created

--- src/main/java/ar/com/ale94/spring_jpa_crud_example/entity/UserEntity.java
+++ src/main/java/ar/com/ale94/spring_jpa_crud_example/entity/UserEntity.java
@@ -1,4 +1,29 @@
 package ar.com.ale94.spring_jpa_crud_example.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity(name = "users")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phone;
+    private LocalDateTime createdAt;
 }
